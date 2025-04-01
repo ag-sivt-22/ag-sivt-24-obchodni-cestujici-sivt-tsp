e6e51f0bb5474f474a5e2adb1a2ccc09fe50f9e6
Pavel Kryl <pavel@kryl.eu>
2025-04-01 10:15:12 +0200

    printing total number of solutions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from graph import Cesta, vyzkousej_cesty
+from graph import Cesta, vyzkousej_cesty, reseni
 from europe import load_europe
 
 evropa = load_europe()
@@ -6,4 +6,4 @@ print(evropa)
 
 cesta = Cesta(["Madrid"], 0)
 vyzkousej_cesty(evropa, cesta)
-print("hotovo")
+print(f"hotovo, nasel jsem {len(reseni)} reseni")
